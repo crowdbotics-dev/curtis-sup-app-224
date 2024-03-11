e365eeb4d4328b1309dd2cacfbe07d407b87926c
Crowdbotics <engineering@crowdbotics.com>
2024-03-11 21:38:06 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "curtis-sup-app-224",
+  url: "https://curtis-sup-app-224.botics.co",
+  api: "https://curtis-sup-app-224.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
